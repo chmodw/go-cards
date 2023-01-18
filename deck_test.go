cbd9137371d0d8eec6d3a3e5d5cbcf2c124d1eea
chamodya wimansha <chamodyawimansha@gmail.com>
2023-01-18 18:35:43 +0530

    card name test added

--- deck_test.go
+++ deck_test.go
@@ -10,4 +10,12 @@ func TestNewDeck(t *testing.T) {
 		t.Errorf("Expected deck length of 16, but got %v", len(d))
 	}
 
+	if d[0] != "Ace of Spades" {
+		t.Errorf("Expected first card of Ace of Spades, but got %v", d[0])
+	}
+
+	if d[len(d)-1] != "Four of Clubs" {
+		t.Errorf("Expected last card of Four of Clubs, but god %v", d[len(d)-1])
+	}
+
 }
